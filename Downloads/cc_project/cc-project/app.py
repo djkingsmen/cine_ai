7a60bcf19ca4d0f814a7c02f945df8d129ffd456
BATHULA DHANUNJAY SURAJ <147162865+djkingsmen@users.noreply.github.com>
2026-02-07 00:37:42 +0530

    Delete Downloads/cc_project/cc-project directory

--- Downloads/cc_project/cc-project/app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import aws_cdk as cdk
-from cloud_governance.cloud_governance_stack import CloudGovernanceStack
-
-app = cdk.App()
-CloudGovernanceStack(app, "CloudGovernanceStack",
-    env=cdk.Environment(account="477345795074", region="eu-north-1")
-)
-app.synth()
